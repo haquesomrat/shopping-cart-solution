9ea6a797a61083c2a1da87a4bf4094790457ab3d
Injamul Haque <ihsomrat.official@gmail.com>
2021-08-21 14:53:10 +0600

    cart is just not ready

---
+++ app.js
@@ -0,0 +1,29 @@
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing == true) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    //update total
+    const prductTotal = document.getElementById(product + '-total');
+    prductTotal.innerText = productNumber * price;
+}
+//handle phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+})
+
+//handle case increase decrease events
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true);
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false);
+});
